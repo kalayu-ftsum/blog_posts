dca54ee2ebc570abe067da0dce59bc3db1638882
Kalayu-Ftsum <kalayuftsumofficial@gmail.com>
2024-11-30 17:18:07 +0100

    Fixed an issue

--- src/components/gallery/OurGallery.jsx
+++ src/components/gallery/OurGallery.jsx
@@ -52,6 +52,7 @@ export default function OurGallery() {
       return () => {if(isotope) {
         isotope.destroy()
     }};
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [isotopeContainer]);
   
     const handleFilterChange = (filterValue) => {
